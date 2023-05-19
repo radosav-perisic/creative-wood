52e58f603ce600e8968477821f222259875785d9
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-19 20:07:48 +0200

    Add loading state and handle API req in useEffect
    Implement data fetching and state management with useEffect and useState

--- creative-wood/src/components/Products.jsx
+++ creative-wood/src/components/Products.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Products = () => {
+    const [data, setData] = useState([])
+    const [filter, setFilter] = useState(data)
+    const [loading, setLoading] = useState(false)
+    let componentMounted = true     
+
+    useEffect(()=>{
+const getProducts = async () => {
+setLoading(true);
+const response= await fetch('https://fakestoreapi.com/products');
+if(componentMounted) {
+    setData(await response.clone().json());
+    setFilter(await response.json());
+    setLoading(false);
+    console.log(filter);
+}
+return () => {
+    componentMounted = false;
+}
+}
+getProducts();
+    },[])
   return (
     <div>
-        
+     <div className='container'>
+        <div className='flex flex-row'>
+            <div className='grid-cols-12 grid'>
+          <h1>Latest Products!</h1>
+            </div>
+
+        </div>
+     </div>
     </div>
   )
 }
