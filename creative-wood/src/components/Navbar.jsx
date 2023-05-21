f76f124e6dba8790225b64da6ca916164a044218
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-21 13:01:39 +0200

    Remove Sign In from Navbar

--- creative-wood/src/components/Navbar.jsx
+++ creative-wood/src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
+
 const Navbar = () => {
   return (
-    <div classNameName=''>
+    <div className=''>
         <div className="fixed place-items-center ">
     <section className="relative mx-auto">
         {/* <? navbar  */}
-      <nav className="flex justify-between bg-gray-900 text-white w-screen">
+      <nav className="flex justify-between bg-gray-700 text-white w-screen">
         <div className="px-5 xl:px-12 py-6 flex w-full items-center">
           <a className="text-3xl font-bold font-heading" href="#">
-            <img className="h-9" src="logo.png" alt="logo"/> 
-            
           </a>
           {/*  Nav Links  */}
           <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
@@ -36,17 +35,11 @@ const Navbar = () => {
                   </span>
                 </span>
             </a>
-            {/*  Sign In / Register       */}
-            <a className="flex items-center hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-            </a>
             
           </div>
         </div>
         {/*  Responsive navbar  */}
-        <a className="xl:hidden flex mr-6 items-center" href="#">
+        <a className="lg:hidden flex mr-6 items-center" href="#">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
