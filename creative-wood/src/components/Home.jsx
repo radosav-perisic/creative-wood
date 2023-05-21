fd4b252014f573a1a01d00e9a127e07da3ee24e6
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-21 12:06:54 +0200

    Modify Home with new section and improved styles

--- creative-wood/src/components/Home.jsx
+++ creative-wood/src/components/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import woodenwall from '../assets/woodenwall.jpg'
+import React from "react";
+import woodenwall from "../assets/woodenwall.jpg";
 
 const Home = () => {
   return (
-    <div className='w-full h-full flex flex-col justify-center items-center'>
-        <div className='border-0 bg-amber-950 text-white'>
-            <img src={woodenwall} className='h-screen w-screen'/>
-
-        </div>
+<div className="bg-gray-200">
+  <div class="bg-gray-600 text-white border-0">
+    <img src={woodenwall} className="flex w-screen h-screen"/>
+    <div class="flex absolute top-[40%] left-[3%] flex-col justify-center items-center ">
+      <div class="container mx-auto px-4">
+        <h5 class="text-[75px] font-bold mb-2">NEW ANIME COLLECTION</h5>
+        <p class="text-2xl text-gray-200  ">We got the best laser cut wood products on the web!</p>
+      </div>
     </div>
-  )
-}
+  </div>
+  <Products />
+</div>
 
-export default Home
+  );
+};
+
+
+export default Home;
