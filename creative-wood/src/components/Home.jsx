3ee1fdfa519663c164a3b1b2b44f4f57c3d064d2
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-21 16:46:59 +0200

    Update background image for Home page

--- creative-wood/src/components/Home.jsx
+++ creative-wood/src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import woodenwall from "../assets/woodenwall.jpg";
+import woodenwall from "../assets/woodenwall.png";
+import Products from "./Products";
 
 const Home = () => {
   return (
 <div className="bg-gray-200">
-  <div class="bg-gray-600 text-white border-0">
-    <img src={woodenwall} className="flex w-screen h-screen"/>
-    <div class="flex absolute top-[40%] left-[3%] flex-col justify-center items-center ">
-      <div class="container mx-auto px-4">
-        <h5 class="text-[75px] font-bold mb-2">NEW ANIME COLLECTION</h5>
-        <p class="text-2xl text-gray-200  ">We got the best laser cut wood products on the web!</p>
+  <div className="bg-gray-600 text-transparent border-0">
+    <img src={woodenwall} className ="flex w-full h-full"/>
+    <div className="flex absolute top-[40%] left-[3%] flex-col justify-center items-center ">
+      <div className="container mx-auto px-4">
+        <h5 className="text-[75px] font-bold mb-2 ">NEW ANIME COLLECTION</h5>
+        <p className="text-2xl text-gray-200  ">We got the best laser cut wood products on the web!</p>
       </div>
     </div>
   </div>
