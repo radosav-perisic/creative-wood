f81cf0f8bc0e0d9f535114b73ee1c3890d088d9d
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-28 11:44:47 +0200

    Add routes to App component

--- creative-wood/src/App.jsx
+++ creative-wood/src/App.jsx
@@ -1,15 +1,22 @@
-import Footer from "./components/Footer"
-import Home from "./components/Home"
+import Home from "./pages/Home"
+import AnimeLamps from "./pages/AnimeLamps"
 import Navbar from "./components/Navbar"
+import { Route, Routes} from 'react-router-dom'
+import Footer from './components/Footer'
+import Cigar from "./pages/Cigar"
 
 function App() {
 
   return (
-    <div>
+    <>
       <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/animelamps" element={<AnimeLamps/>} />
+      <Route exact path='/cigarboxes' element={<Cigar />}/>
+      </Routes>
+      <Footer/>
+    </>
   )
 }
 
