cec224133131346f2b663ca9b503ebbd99ae1dc4
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-28 11:48:58 +0200

    Create handleCart component

---
+++ creative-wood/src/redux/reducer/handleCart.js
@@ -0,0 +1,24 @@
+const handleCart = [];
+
+const addItems = (state = handleCart, action) => {
+    switch (action.type) {
+        case "ADDITEM" : return [
+            ...state,
+            action.payload
+        ]
+        break;
+
+        case "DELITEM" :
+            return state = state.filter((x)=>{
+                return x.id !== action.payload.id
+            })
+        break;
+
+        default: return state;
+        break;
+
+        
+    }
+}
+
+export default addItems;
