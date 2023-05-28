9d48ead057e47dbb4e12ad309343fe6756200c71
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-28 13:27:42 +0200

    Create Cart component

--- creative-wood/src/redux/reducer/handleCart.js
+++ creative-wood/src/redux/reducer/handleCart.js
@@ -1,24 +1,43 @@
-const handleCart = [];
+const cart = [];
 
-const addItems = (state = handleCart, action) => {
+const addItems = (state = cart, action) => {
+    const product = action.payload;
     switch (action.type) {
-        case "ADDITEM" : return [
-            ...state,
-            action.payload
-        ]
+        case "ADDITEM" : 
+        
+        const exist = state.find((x) => x.id === product.id);
+        if(exist) {
+            return state.map((x) => 
+            x.id === product.id ? {...x, qty: x.qty + 1} : x)
+        }else{
+            const product = action.payload;
+            return [
+                ...state,
+                {
+                    ...product,
+                    qty:1, 
+                }
+            ]
+        }
         break;
+        
 
         case "DELITEM" :
-            return state = state.filter((x)=>{
-                return x.id !== action.payload.id
-            })
-        break;
-
-        default: return state;
-        break;
+            const exist1 =  state.find((x) => x.id === product.id);
+            if(exist1.qty === 1) {
+                return state.filter((x) => x.id !== exist1.id);
+            }else {
+                return state.map((x) => 
+                x.id === product.id ? {...x, qty: x.qty-1} : x )
+            }
+            break;
+            default:
+                return state;
+                break;
+            }      
 
         
     }
-}
+
 
 export default addItems;
--- creative-wood/src/redux/store.js
+++ creative-wood/src/redux/store.js
@@ -1,5 +1,5 @@
 import { legacy_createStore } from "redux";
-import rootReducers from "./reducers";
+import rootReducers from "./reducer/reducers";
 
 const store = legacy_createStore(rootReducers);
 
