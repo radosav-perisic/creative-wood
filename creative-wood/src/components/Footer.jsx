d375814419af6fe8cd22c264c1a2e18b2ce2dc54
Radosav Perisic <radosavperisic00@gmail.com>
2023-05-31 19:42:39 +0200

    Add Trademark and social icons to Footer component

--- creative-wood/src/components/Footer.jsx
+++ creative-wood/src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook, FaInstagram} from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full h-28 flex items-center justify-center bg-[#191717] px-6 text-white">
+      
+      <div className="flex space-x-10 items-center">
+      <p className="text-[13px] text-gray-300/50 ml-auto">
+      © 2023 Creative Wood. All Rights Reserved.
+      </p>
 
-export default Footer
+      <div className='space-x-3 flex'>
+        <a href="#" className="text-2xl hover:scale-125 duration-300">
+          <FaInstagram />
+        </a>
+        <a href="#" className="text-2xl hover:scale-125 duration-300">
+          <FaFacebook />
+        </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
