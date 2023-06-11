6b0e5e7a3aa6f35f0715e3f7d628d0a46f8f0d7c
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-11 14:43:16 +0200

    Update AnimeProducts to use Redux
    Update AnimeProducts to use Redux useDispatch() for mapping products

--- creative-wood/src/components/AnimeProducts.jsx
+++ creative-wood/src/components/AnimeProducts.jsx
@@ -1,53 +1,58 @@
 import React from "react";
-import products from '../animeLamps.json'
+import products from "../products/animeLamps.json";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actions";
 
 const AnimeProducts = () => {
 
-
+ const dispatch = useDispatch();
+ const addProduct = (product) => {
+  dispatch(addToCart(product))
+ }
 
   const ShowAnimeProducts = () => {
     return (
       <>
-        
         {products.map((product) => {
           return (
-            
-             
-                <div key={product.id} className="mx-auto justify-center items-center shadow-2xl container p-2 shadow-black bg-cyan-100/10 group rounded-md my-3 lg:h-full h-[170px] lg:w-[270px] w-36 text-center">
-                  <img
-                    src={product.image}
-                    className="lg:h-[210px] h-[110px] lg:group-hover:opacity-0 focus:opacity-0 delay-[70ms] duration-300 object-contain mx-auto rounded-md"
-                    alt={product.title}
+            <div
+              key={product.id}
+              className="mx-auto justify-center group outline outline-white/10 items-center container shadow-2xl p-1 shadow-black bg-white/5 backdrop-blur-[3px] rounded-2xl my-3 h-52 lg:h-full lg:w-[270px] w-36 text-center"
+            >
+              <div
+                style={{
+                  "--image-url": `url(${product.image})`,
+                  "--image-hover-url": `url(${product.imageHover})`,
+                                 
+                }}
+                className="lg:h-[210px] h-[110px] bg-contain bg-center bg-no-repeat hover:scale-[1.2] bg-[image:var(--image-hover-url)] lg:bg-[image:var(--image-url)] group-hover:bg-[image:var(--image-hover-url)] delay-[30ms] group duration-300 object-contain mx-auto rounded-md"
+                alt={product.title}
+              ></div>
 
-                  />
-                   <img
-                    src={product.imageHover}
-                    className="lg:h-[210px] h-[110px] lg:hover:scale-[1.20] focus::scale-[1.20] duration-[350ms] opacity-0 absolute group-hover:opacity-100 w-full object-contain mx-auto rounded-md"
-                    alt={product.title}
-
-                  />
-                  <div className="rounded-lg">
-                    <h5 className="text-[1.6rem] text-gray-100 font-medium mb-1">
-                      {product.title}
-                    </h5>
-                    <p className=" text-[#c53838] text-md font-bold">RSD {product.price}</p>
-                    <button
-                      href="#"
-                      className=" flex flex-row mt-2 mx-auto  bg-[#261111] px-1 lg:px-3 py-1 hover:bg-[#600d0d] duration-300 text-sm lg:text-lg text-white rounded-md mb-1 "
-                    >
-                      Dodaj u korpu
-                    </button>
-                  </div>
-                </div>              
-            
+              <h5 className="lg:text-[1.32rem] text-md text-gray-100 font-medium mb-1">
+                {product.title}
+              </h5>
+              <p className=" text-[#c53838] lg:text-lg text-sm font-bold">
+                RSD {product.price}
+              </p>
+              <button
+                href="#"
+                className="flex flex-row lg:mt-5 mt-2 mx-auto  bg-[#261111] px-1 lg:px-3 lg:py-3 py-1 hover:bg-[#600d0d] duration-300 text-sm lg:text-lg text-white rounded-md"
+                onClick={() => addProduct(product)}
+              >
+                Dodaj u korpu
+              </button>
+            </div>
           );
         })}
       </>
     );
   };
 
-  return (  
-        <ShowAnimeProducts />
+  return (
+    <div className="grid md:grid-cols-4 grid-cols-2 gap-8 lg:gap-16 py-8 mx-auto mt-56 lg:mt-96">
+      <ShowAnimeProducts />
+    </div>
   );
 };
 
