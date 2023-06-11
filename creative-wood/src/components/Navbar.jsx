cba5ee36c9e31a06f0a4a330d70a8b1e579063ee
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-11 14:37:21 +0200

    Update Navbar to use useSelector
    Update Navbar to use useSelector for tracking item quantity in cart

--- creative-wood/src/components/Navbar.jsx
+++ creative-wood/src/components/Navbar.jsx
@@ -1,114 +1,84 @@
 import React from "react";
-import Example from "./Categories";
+import Kolekcije from "./Categories";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {BsCart3} from 'react-icons/bs'
+import { Link } from "react-scroll";
+
+
 
 const Navbar = () => {
+const state = useSelector((state) => state.addItems)
+const totalQty = state.reduce((total, item) => total + item.qty, 0);
+
+const handleClick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+
   return (
     <div className="">
       <div className="fixed place-items-center z-10">
         <section className="relative mx-auto">
-          {/* <? navbar  */}
+          {/* Navbar  */}
           <nav className="flex justify-between bg-transparent backdrop-blur-[5px] shadow-md shadow-black/20 text-white w-screen">
-            <div className="px-5 xl:px-12 lg:py-5 py-7 flex w-full items-center">
-              <a className="text-3xl font-bold font-heading" href="#"></a>
+            <div className="lg:px-5 xl:px-12 lg:py-5 py-7 flex w-full items-center">
               {/*  Nav Links  */}
               <ul className="hidden md:flex px-4 mx-auto gap-14 font-semibold font-heading space-x-12">
                 <li>
-                  <NavLink className="text-base text-[#ffffff]  font-bold" as={NavLink} to="/">
+                  <NavLink onClick={handleClick} className="text-base hover:text-blue-500/80 duration-200 text-[#ffffff]/90  font-bold" as={NavLink} to="/">
                     Home
                   </NavLink>
                 </li>
                 <li>
-                <Example />
-                </li>
-                <li className="text-base text-[#ffffff]  font-bold" as={NavLink} to="/collections">
-                 
-                    Kolekcije
-                  
+                <Kolekcije />
                 </li>
                 <li>
-                <NavLink className="text-base text-[#ffffff]  font-bold" as={NavLink} to="/about">
+                <NavLink className="text-base hover:text-blue-500/80 duration-200 text-[#ffffff]/90 font-bold" to="about">
                     O Nama
                   </NavLink>
                 </li>
               </ul>
               {/*  Header Icons  */}
               <div className="hidden xl:flex place-items-center space-x-5 items-center">
-                <a className="hover:text-gray-200" href="#">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    />
-                  </svg>
-                </a>
-                <a className="flex items-center hover:text-gray-200" href="#">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    />
-                  </svg>
-                  <span className="flex absolute -mt-5 ml-4">
-                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-[#e61c1c] opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-3 bg-[#e61c1c]"></span>
+
+   
+                <NavLink onClick={handleClick}  as={NavLink}
+                to='/cart'><BsCart3 className="h-7 w-7"/>
+                  <span className="flex absolute text-center -mt-8 ml-5">
+                    <span className="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-[#e61c1c] text-white text-lg opacity-75"></span>
+                    <span className="relative text-center text-sm font-medium rounded-full h-4 w-4 bg-[#e61c1c]">{totalQty}</span>
                   </span>
-                </a>
+             </NavLink>
               </div>
             </div>
             {/*  Responsive navbar  */}
-            <a className="lg:hidden flex mr-6 items-center" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 hover:text-gray-200"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="flex absolute -mt-5 ml-4">
-                <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-[#e61c1c] opacity-75"></span>
-                <span className="relative inline-flex rounded-full h-3 w-3 bg-[#e61c1c]"></span>
+            <ul className="lg:hidden gap-10 inline-flex mx-auto items-center font-semibold font-heading ">
+                <li>
+                  <NavLink onClick={handleClick} className="text-xs text-[#ffffff]/90 font-bold" as={NavLink} to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                <Kolekcije />
+                </li>
+                <li>
+                <NavLink onClick={handleClick} className="text-xs whitespace-nowrap text-[#ffffff]/90 font-bold" as={NavLink} to="/about">
+                    O Nama
+                  </NavLink>
+                </li>
+            <div className="lg:hidden mr-6 items-center" href="#">
+            <NavLink  as={NavLink}
+                to='/cart'><BsCart3 className="lg:h-7 lg:w-7 w-6 h-6"/></NavLink>
+              <span className="flex absolute -mt-8 lg:-mt-5 ml-4">
+                <span className="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-[#e61c1c] text-white text-lg opacity-75"></span>
+                <span className="relative flex items-center text-sm font-medium justify-center rounded-full h-4 w-4 bg-[#e61c1c]">{totalQty}</span>
               </span>
-            </a>
-            <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 hover:text-gray-200"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            </a>
+            </div>         
+              </ul>
           </nav>
         </section>
       </div>
