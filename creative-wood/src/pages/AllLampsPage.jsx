17b3ec44c79a4077381d809415cf30100bf68dac
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-11 14:36:35 +0200

    Update allLamps page

--- creative-wood/src/pages/AllLampsPage.jsx
+++ creative-wood/src/pages/AllLampsPage.jsx
@@ -1,28 +1,24 @@
 import React from "react";
 import AllLamps from "../components/AllLamps";
-import AllLampsWall from "../assets/AllLampsWall.png";
-import AllLampsWallMobile from "../assets/AllLampsWallMobile.png";
+// import AllLampsWall from "../assets/AllLampsWall.png";
+// import AllLampsWallMobile from "../assets/AllLampsWallMobile.png";
+import woodenWallLong from '../assets/woodenWallLong.png'
 import { useMediaQuery } from "@react-hook/media-query";
-import allLampsImage from "../assets/allLampsImagepng";
+// import allLampsImage from "../assets/allLampsImagepng";
 
 const AllLampsPage = () => {
   const backgrounds = useMediaQuery("(min-width: 768px)")
-    ? AllLampsWall
-    : AllLampsWallMobile;
+    ? woodenWallLong
+    : woodenWallLong;
 
   return (
-    <div
+    <div name='alllamps'
       style={{
         backgroundImage: `url(${backgrounds})`,
       }}
       className="lg:bg-cover bg-top bg-cover h-full bg-no-repeat lg:h-full w-full"
     >
-      <div className="w-full h-[280px] absolute mt-16">
-        <img
-          src={allLampsImage}
-          className="h-full object-cover w-full -mt-10  md:hidden"
-        />
-      </div>
+
       <div className="w-full h-full justify-center flex flex-col p-4 mx-auto max-w-[1500px]">
         <h2 className="text-6xl mt-16 font-bold text-white tracking-wide hidden md:flex">
           Boris Kralj
