68912041eaca352849c98df49aca67f7b31244ae
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-11 14:46:42 +0200

    Update AnimeLamps.jsx

--- creative-wood/src/pages/AnimeLamps.jsx
+++ creative-wood/src/pages/AnimeLamps.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import AnimeProducts from '../components/AnimeProducts';
+import React from "react";
+import AnimeProducts from "../components/AnimeProducts";
 import AnimeWall from "../assets/AnimeWall.png";
-import { useMediaQuery } from '@react-hook/media-query';
+import AnimeWallMobile from "../assets/blackwall.png";
+import { useMediaQuery } from "@react-hook/media-query";
+import animeSection from "../assets/1.png";
 
 const AnimeLamps = () => {
- 
-  const backgrounds = useMediaQuery('(min-width: 768px)') ? AnimeWall : AnimeWall;
+  const backgrounds = useMediaQuery("(min-width: 768px)")
+    ? AnimeWall
+    : AnimeWallMobile;
 
   return (
-    <div  style={{
-      backgroundImage: `url(${backgrounds})`,
-      }} className="lg:bg-cover bg-top bg-contain bg-no-repeat lg:h-full w-full">
-    <div className="w-full h-full justify-center flex flex-col p-4 mx-auto max-w-[1500px]">
-      <h2 className="text-6xl mt-16 font-bold text-white tracking-wide">Geopasinci</h2>     
-         <AnimeProducts />
+    <div
+      style={{
+        backgroundImage: `url(${backgrounds})`,
+      }}
+      className="lg:bg-cover bg-top bg-cover h-full bg-no-repeat lg:h-full w-full"
+    >
+      <div className="w-full h-[280px] absolute mt-16">
+        <img
+          src={animeSection}
+          className="h-full object-cover w-full -mt-10  md:hidden"
+        />
+      </div>
+      <div className="w-full h-full justify-center flex flex-col p-4 mx-auto max-w-[1500px]">
+        <AnimeProducts />
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default AnimeLamps
+export default AnimeLamps;
