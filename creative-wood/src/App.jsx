c3ea88f9cab0f90632761b319167ebc809035a5f
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-11 14:53:21 +0200

    Add additional routes to App.jsx

--- creative-wood/src/App.jsx
+++ creative-wood/src/App.jsx
@@ -3,7 +3,13 @@ import AnimeLamps from "./pages/AnimeLamps"
 import Navbar from "./components/Navbar"
 import { Route, Routes} from 'react-router-dom'
 import Footer from './components/Footer'
-import Cigar from "./pages/Cigar"
+// import Cigar from "./pages/Cigar"
+import Cart from "./components/Cart"
+import Checkout from "./components/Checkout"
+import About from "./pages/About"
+import Clocks from "./pages/Clocks"
+import AllLampsPage from "./pages/AllLampsPage"
+// import AllLampsPage from "./pages/AllLampsPage"
 
 function App() {
 
@@ -13,7 +19,13 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home/>} />
       <Route exact path="/animelamps" element={<AnimeLamps/>} />
-      <Route exact path='/cigarboxes' element={<Cigar />}/>
+      <Route exact path='/cart' element={<Cart />}/>
+      <Route exact path='/about' element={<About />}/>
+      {/* <Route exact path='/cigarboxes' element={<Cigar />}/> */}
+      <Route exact path='/checkout' element={<Checkout />}/>
+      <Route exact path='/clocks' element={<Clocks />}/>
+      <Route exact path='/alllamps' element={<AllLampsPage />}/>
+      {/* <Route exact path='/AllLampsPage' element={<AllLampsPage />}/> */}
       </Routes>
       <Footer/>
     </>
