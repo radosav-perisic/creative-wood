3690860856b885dddb6a281df012ae5e69b3c8d4
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-14 13:21:09 +0200

    Update background and Link button styles
    Update background and Link button styles, also added the footer component on the very bottom of the page

--- creative-wood/src/pages/Home.jsx
+++ creative-wood/src/pages/Home.jsx
@@ -2,36 +2,34 @@ import React from "react";
 import home from "../assets/hero.png";
 import homeMobile from "../assets/homemob.png";
 import { Link } from "react-scroll";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import Services from "../components/Services";
+import Footer from "../components/Footer";
 import { useMediaQuery } from "@react-hook/media-query";
-
 
 const Home = () => {
   const backgrounds = useMediaQuery("(min-width: 768px)") ? home : homeMobile;
+  const bgStyle = {
+    backgroundImage: `url(${backgrounds})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
 
   return (
-    <div className="bg-gray-200">
-      <div className="bg-gray-600 text-transparent border-0">
+    <div className="w-full h-screen relative">
+      <div style={bgStyle} className="w-full h-full" />
+      <div className="absolute bottom-1/4  left-1/2 transform -translate-x-1/2">
         <Link
-          className="text-white outline-[6px] outline outline-blue-700/80 hover:bg-blue-700/80 tracking-widest rounded-full py-1 px-3 absolute justify-center cursor-pointer text-center md:top-[80%] top-[80%] left-[36.5%] md:left-[45.7%] text-[1rem] md:text-[1.6rem] duration-300"
+          className="text-white color-changing-link font-semibold cursor-pointer ring-[7px] ring-blue-800  hover:bg-blue-950  tracking-widest rounded-full py-1 px-3 text-center text-[1.2  rem] md:text-[1.9rem] duration-300"
           to="services"
           smooth={true}
           duration={500}
         >
           USLUGE
         </Link>
-
-        <Link to="services" smooth={true} duration={500}>
-          <ChevronDownIcon
-            className="lg:h-16 ml-[58px] md:ml-0 h-12 cursor-pointer animate-down mt-5 text-white absolute top-[83%] lg:left-[48.1%] left-[27.2%]"
-            aria-hidden="true"
-          />
-        </Link>
-
-        <img src={backgrounds} className="flex w-full h-screen lg:h-full" />
       </div>
       <Services />
+      <Footer />
     </div>
   );
 };
