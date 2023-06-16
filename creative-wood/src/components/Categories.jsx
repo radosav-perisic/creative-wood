787069688e73510ba5b75c098cf362f9f67ccf3c
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-16 15:41:50 +0200

    Add handleClick function to all links
    Add handleClick function to all links in Categories & change the bg color of all links and the menu in Categories

--- creative-wood/src/components/Categories.jsx
+++ creative-wood/src/components/Categories.jsx
@@ -6,15 +6,25 @@ import { NavLink } from "react-router-dom";
 
 function classNames(...classes) {
 
+   
+
   return classes.filter(Boolean).join(" ");
 }
 
 export default function Kolekcije() {
 
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "auto",
+    });
+  };
+
+
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex text-[#ffffff] outline outline-blue-500/80 lg:py-0.3 px-0.5 lg:px-1.5  font-bold w-full justify-center gap-x-1.5 rounded-md bg-transparent text-sm lg:text-base hover:bg-blue-500/80 duration-300">
+        <Menu.Button className="inline-flex tracking-wide text-[#ffffff] outline outline-white/80 lg:py-0.3 px-0.5 lg:px-1.5  font-bold w-full justify-center gap-x-1.5 rounded-md bg-transparent text-sm lg:text-base hover:bg-[#1d4ed8] duration-300">
           Kolekcije
           <ChevronDownIcon
             className="-mr-1 mt-[0.04rem] lg:h-6 lg:w-6 h-5 w-5 text-white"
@@ -32,15 +42,15 @@ export default function Kolekcije() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right bg-white shadow-lg">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right bg-[#1d4ed8] shadow-lg">
           <div className="pb-1.5">
             <Menu.Item>
               {({ active }) => (
-                <NavLink
+                <NavLink onClick={handleClick}
                 as={NavLink}
                   to='/animelamps'
                   className={classNames(
-                    active ? "hover:text-red-500 duration-150" : "bg-gray-100  text-gray-900",
+                    active ? "hover:text-white duration-150" : "bg-[#1d4ed8]/80  text-gray-900",
                     "block pl-3.5 pr-4 pt-2  text-sm"
                   )}
                 >
@@ -50,13 +60,13 @@ export default function Kolekcije() {
                 </NavLink>
               )}
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item onClick={handleClick}>
               {({ active }) => (
-                <NavLink
+                <NavLink 
                 as={NavLink}
                 to='/alllamps'
                   className={classNames(
-                    active ? "hover:text-red-500 duration-150" : "bg-gray-100  text-gray-900",
+                    active ? "hover:text-white bg-[#1d4ed8] duration-150" : "bg-[#1d4ed8]  text-gray-900",
                     "block px-4 my-2 text-sm ml-0"
                   )}
                 >
@@ -68,11 +78,11 @@ export default function Kolekcije() {
             </Menu.Item>
             <Menu.Item>
               {({ active }) => (
-                <NavLink
+                <NavLink onClick={handleClick}
                 as={NavLink}
                 to='/clocks'
                   className={classNames(
-                    active ? "hover:text-red-500 duration-150" : "bg-gray-100  text-gray-900",
+                    active ? "hover:text-white duration-150" : "bg-[#1d4ed8]  text-gray-900",
                     "block px-4 my-2 text-sm ml-0"
                   )}
                 >
