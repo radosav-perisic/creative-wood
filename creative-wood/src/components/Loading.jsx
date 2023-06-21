347b2845233b97030faa6658f893940dbe724376
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-21 18:02:11 +0200

    Implement useEffect api fetching logic in Loading
    Fetch API using the useEffect hook to display a loading spinner to the user while the products load in the background to improve user experience by making the users page load time seem smoother

--- creative-wood/src/components/Loading.jsx
+++ creative-wood/src/components/Loading.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
-}
+const Loading = ({ children, apiEndpoint }) => {
+  const [isLoading, setIsLoading] = useState(true);
 
-export default Loading
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiEndpoint);
+        const data = await response.json();
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error while loading data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+    };
+  }, [apiEndpoint]);
+
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <div className="spinner" />
+        <div className="backdrop" />
+      </div>
+    );
+  }
+
+  return children;
+};
+
+export default Loading;
