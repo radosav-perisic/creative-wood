31627289206c235b88a3baec3a0d65d0a2ba7f5a
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-26 12:18:34 +0200

    Add new Logo image and improve responsiveness
    Add new Logo image and improve responsiveness in AllLampsPage page

--- creative-wood/src/pages/AllLampsPage.jsx
+++ creative-wood/src/pages/AllLampsPage.jsx
@@ -1,37 +1,43 @@
 import React from "react";
 import AllLamps from "../components/AllLamps";
-// import AllLampsWall from "../assets/AllLampsWall.png";
-// import AllLampsWallMobile from "../assets/AllLampsWallMobile.png";
-import background from '../assets/ledlampebackground.png'
 import blackwall from '../assets/blackwall.png'
-import Logo from '../assets/2.png'
-import { useMediaQuery } from "@react-hook/media-query";
-import Footer from "../components/Footer";
-// import allLampsImage from "../assets/allLampsImagepng";
+import Logo from '../assets/CreativeWoodLogo.png'
+import alllampslogo from '../assets/alllampslogo.png'
+
+
 
 const AllLampsPage = () => {
-  const backgrounds = useMediaQuery("(min-width: 768px)")
-    ? background
-    : blackwall;
+
 
   return (
     <div name='alllamps'
       style={{
-        backgroundImage: `url(${backgrounds})`,
+        backgroundImage: `url(${blackwall})`,
       }}
-      className="lg:bg-cover bg-top bg-cover h-full bg-no-repeat lg:h-full w-full"
+      className="lg:bg-cover bg-top bg-cover justify-center items-center h-full bg-no-repeat lg:h-full w-full"
     >
-<div className="w-full h-[280px] md:hidden -mt-40 absolute"><img
-          src={Logo}
-          className="h-[250px] object-contain w-full"
-        /></div>
-      <div className="w-full h-full justify-center flex flex-col p-4 mx-auto max-w-[1500px]">
-        <h2 className="md:text-2xl text-sm lg:leading-8 leading-4 tracking-tight  font-bold lg:w-[960px] text-white flex">
-          * Sve nase lampe dolaze sa RGB-LED svetlima i svaki korisnik preko daljinskog upravljaca koji se dobija u kompletu sa lampom moze da menja boju svetla po zelji 
-        </h2>
+      
+      <div className="w-full h-full flex flex-col lg:flex-row items-center justify-center lg:justify-start">
+        <div className="w-[20%] h-full ml-2 hidden lg:flex items-start flex-row">
+          <span className="text-[#52d6eb] text-3xl "> * </span>
+          <h2 style={{ lineHeight: "1.1" }} className="text-white/90 md:text-lg text-sm justify-end tracking-tight font-bold">
+          Naše lampe dolaze sa ugrađenim RGB-LED svetlima, omogućavajući korisnicima da putem priloženog daljinskog upravljača promene boju svetla po želji.
+          </h2>
+        </div>
+        <div className="md:w-[60%] w-full  mt-10 lg:m-0 flex justify-center">
+          <img src={alllampslogo} className="md:h-full object-cover w-full" />
+        </div>
+        <div className="md:w-[60%] sm:w-[90%] ml-2 lg:hidden flex place-items-start items-start mb-5 flex-row justify-start">
+          
+          <h2 style={{ lineHeight: "0.9" }} className="text-white/90 text-sm md:text-lg tracking-tight font-bold"><span className="text-[#52d6eb] text-[22px] ">*</span>
+          Naše lampe dolaze sa ugrađenim RGB-LED svetlima, omogućavajući korisnicima da putem priloženog daljinskog upravljača promene boju svetla po želji.
+          </h2>
+        </div>
+      </div>
+      
+      <div className="w-full h-full pb-10 justify-center flex flex-col mx-auto max-w-[1500px]">
         <AllLamps />
       </div>
-      <Footer />
     </div>
   );
 };
