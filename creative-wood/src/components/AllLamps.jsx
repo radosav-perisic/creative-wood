58faaeacf5ccff7051512a611dedaa04b417ada2
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-26 12:18:51 +0200

    Update AllLamps component styles

--- creative-wood/src/components/AllLamps.jsx
+++ creative-wood/src/components/AllLamps.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import products from "../products/allLedLamps.json";
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { addToCart } from "../redux/actions";
 
 const AllLamps = () => {
     const dispatch = useDispatch();
@@ -18,7 +20,7 @@ const AllLamps = () => {
     return (
       <>
         {products.map((product) => {
-          const imageId = `image-${product.id}`; // Generate unique id for each product image
+          const imageId = `image-${product.id}`;
           return (
             <div
               key={product.id}
@@ -32,18 +34,18 @@ const AllLamps = () => {
                 }}
                 className="lg:h-[190px] h-[160px] bg-contain bg-center bg-no-repeat hover:scale-[1.15] bg-[image:var(--image-hover-url)] lg:bg-[image:var(--image-url)] group-hover:bg-[image:var(--image-hover-url)] delay-[30ms] group duration-300 object-contain mx-auto rounded-md"
                 alt={product.title}
-                onClick={() => openFullscreen(imageId)} // Pass imageId as parameter
+                onClick={() => openFullscreen(imageId)}
               ><link rel="preload" as="image" href={product.imageHover} /></div>
 
-              <h5 className="lg:text-[1.6rem] text-lg text-gray-100 font-medium mb-1">
+              <h5 className="lg:text-[1.32rem]  text-sm text-gray-100 font-medium mb-1">
                 {product.title}
               </h5>
-              <p className=" text-[#c53838] lg:text-md text-sm font-bold">
+              <p className=" text-[#c53838] lg:text-lg text-sm font-bold">
                 RSD {product.price}
               </p>
               <button
                 href="#"
-                className="flex flex-row lg:mt-5 mt-2 mx-auto  bg-[#261111] px-1 lg:px-3 lg:py-3 py-1 hover:bg-[#600d0d] duration-300 text-sm lg:text-lg text-white rounded-md"
+                className="flex flex-row lg:mt-5 mt-2 mx-auto mb-6 bg-[#920f0f] px-2 lg:px-3 lg:py-3 py-2 hover:bg-[#600d0d] duration-300 text-sm lg:text-lg text-white rounded-md"
                 onClick={() => addProduct(product)}
               >
                 Dodaj u korpu
@@ -56,7 +58,7 @@ const AllLamps = () => {
 }
 
 return (
-    <div className="grid md:grid-cols-4 grid-cols-2 gap-8 lg:gap-16 py-8 mx-auto mt-56 lg:mt-96">
+    <div className="grid md:grid-cols-5 grid-cols-2 gap-8 lg:gap-16 py-8 mx-auto">
       <ShowAllLamps />
     </div>
   );
