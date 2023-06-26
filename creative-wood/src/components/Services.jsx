99dc0534cf5a8b4bf474d166ca7e0f3c5ae0ff17
Radosav Perisic <radosavperisic00@gmail.com>
2023-06-26 11:25:26 +0200

    Improve responsiveness of Services
    Improve responsiveness and layout for all screens

--- creative-wood/src/components/Services.jsx
+++ creative-wood/src/components/Services.jsx
@@ -37,9 +37,9 @@ const Services = () => {
       <div className="flex justify-center">
       <h1 className="text-white font-medium text-7xl text-center justify-center tracking-[0.2em] hidden md:block" style={{ transform: 'translateY(8rem)' }}>USLUGE</h1>
       </div>
-      <div className="flex items-end justify-center h-screen">
+      <div className="flex md:items-end lg:gap-0 justify-center h-screen">
       
-        <div className="items-center justify-items-center md:grid-cols-3 lg:gap-x-[8rem] md:gap-y-0 gap-y-[2rem] md:grid flex-cols-3 flex flex-col lg:gap-6">
+        <div className="items-center justify-items-center justify-end md:justify-center md:grid-cols-3 md:gap-x-[2rem] lg:gap-x-[8rem] md:gap-y-2 gap-y-[1rem] md:grid flex-cols-3 flex flex-col lg:gap-6">
            <NavLink to='/animelamps' onClick={handleClick} ><img src={anime} alt="Anime" className="color-changing-image h-auto max-w-[120px]" /></NavLink>
           <h2 className="text-xl font-bold text-white block md:hidden">Izrada Anime LED Lampi</h2>
           <NavLink to='/clocks' onClick={handleClick}><img src={clock} alt="Clock" className="color-changing-image h-auto max-w-[120px]" /></NavLink>
@@ -53,7 +53,7 @@ const Services = () => {
 
         </div>
       </div>
-      <div className="hidden lg:flex-grow lg:flex h-screen justify-center items-center">
+      <div className="hidden md:flex-grow md:flex h-screen md:items-start  justify-center lg:items-center">
         <Products />
       </div>
     </div>
