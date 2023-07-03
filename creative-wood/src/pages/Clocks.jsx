ffc2c10d3d767eebea0b62144338c9de44129c2a
Radosav Perisic <radosavperisic00@gmail.com>
2023-07-03 20:33:21 +0200

    Update Clock page
    Refined the text layout, font styles, and sizes to enhance readability and make the information more accessible to users.
    Made the heading "ZIDNI SATOVI" more prominent with an uppercase styling and a larger font size (40px).
    Employed descriptive and concise sentences to convey key information about the wall clocks' availability, material, and size.

--- creative-wood/src/pages/Clocks.jsx
+++ creative-wood/src/pages/Clocks.jsx
@@ -9,21 +9,24 @@ const Clocks = () => {
 
   return (
     <div
-      className="bg-blue-800/90 h-full lg:h-full w-full"
+      className="bg-[#FFFDFA] h-full lg:h-full w-full"
     >
-        <div className="w-full mx-auto lg:pt-20 pt-10 max-w-[1500px] h-full pb-10 flex flex-col lg:flex-row items-center justify-center lg:justify-start">
-        <div className="md:w-2/4 w-4/5 border-4 shadow-2xl shadow-black/80 rounded-3xl border-gray-700 mt-10 lg:m-0 flex justify-center">
-          <img src={clocksImage} className="md:h-full object-cover rounded-3xl w-full" />
-        </div>
-        <div className="w-2/4 h-full bg-transparent ml-2 hidden shadow-2xl shadow-black/80 rounded-3xl p-3 py-2 lg:flex items-end justify-center flex-row">
+      <div className=" content-div">
+        <div className="w-full  mx-auto lg:pt-24 pt-10 max-w-[1500px] h-full pb-10 flex flex-col lg:flex-row items-center justify-center lg:justify-start">
+        
+        <div className="w-2/4 h-full pr-28 bg-transparent ml-2 hidden mx-auto lg:flex items-end justify-center flex-row">
           
-          <h2 style={{ lineHeight: "1.1" }} className="text-white text-[33px] justify-end font-bold">
-          <h3 className="text-center text-[#422511f4] uppercase tracking-wide text-[35px]">Ekskluzivni drveni satovi sa unikatnim laserskim sečenjem</h3>
-           <p className="my-5"><span className="text-amber-950 font-black">•</span> Dostupni u elegantnoj <span className=" underline underline-offset-4 decoration-amber-950/80"></span>crnoj, čistoj beloj i prirodnoj braon boji</p>
-           <p className="my-5"><span className="text-amber-950 font-black">•</span> Materijal: <span className=" underline underline-offset-4 decoration-amber-950/80">Lesonit</span></p>
-           <p className="my-5"><span className="text-amber-950 font-black">•</span> Prečnik svih satova:  <span className=" underline underline-offset-4 decoration-amber-950/80">40cm</span></p>
-          <p className="my-5"><span className="text-amber-950 font-black">•</span> Za <span className=" underline underline-offset-4 decoration-amber-950/80">dodatne boje i personalizaciju</span>, kontaktirajte nas putem e-maila (dodatna nadoknada se može primeniti)</p>
+          <h2 style={{ lineHeight: "1.1" }} className="text-black/90 text-[20px] justify-end">
+          <h3 className="text-start text-black uppercase tracking-wide text-[40px] font-semibold">ZIDNI SATOVI</h3>
+           <p className="my-4"> Dostupni u elegantnoj <span className=" font-bold ">crnoj, čistoj beloj i prirodnoj braon boji</span>. Ovi satovi će se savršeno uklopiti u bilo koji enterijer i pružiti vam elegantan dodatak za vaš dom.</p>
+           <p className="my-4"> Materijal: <span className=" font-bold ">Lesonit</span>. Izrađeni su od visokokvalitetnog materijala - lesonita. Ovaj materijal je izdržljiv i lagan, što ga čini idealnim za satove koji će biti istovremeno funkcionalni i dekorativni.</p>
+           <p className="my-4"> Prečnik svih satova:  <span className=" font-bold ">40cm</span>. Svi satovi imaju prečnik od 40cm, što ih čini dovoljno velikim da budu primetni, ali ne previše dominantnim, idealnim za bilo koju prostoriju u kući ili kancelariji.</p>
+          <p className="my-4"> Za <span className=" font-bold ">dodatne boje i personalizaciju</span>, kontaktirajte nas putem e-maila. Naša ekipa će vam pružiti informacije o opcijama i mogućnostima, uz dodatnu nadoknadu za personalizaciju.</p>
           </h2>
+         
+        </div>
+        <div className="md:w-2/4 w-4/5 shadow-2xl shadow-black/50 mt-10 my-10 flex justify-center">
+          <img src={clocksImage} className="md:h-full object-cover  w-full" />
         </div>
         <div className="md:w-[60%] sm:w-[90%] ml-2 lg:hidden flex place-items-start items-start mb-5 flex-row justify-start">
           
@@ -32,7 +35,8 @@ const Clocks = () => {
           </h2>
         </div>
       </div>
-      <div className="w-full h-full pb-10 justify-center flex flex-col mx-auto max-w-[1500px]">
+      </div>
+      <div className="w-full h-full py-10 justify-center flex flex-col mx-auto max-w-[1500px]">
         <ClocksComponent />
       </div>
     </div>
