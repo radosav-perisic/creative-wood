366404232c5707c8ec70d8d9f78e2731fae5edb4
Radosav Perisic <radosavperisic00@gmail.com>
2024-08-29 10:59:39 +0200

    Services component translation

--- creative-wood/src/components/Services.jsx
+++ creative-wood/src/components/Services.jsx
@@ -34,22 +34,22 @@ const Services = () => {
       className="w-full h-screen flex flex-col"
     >
       <div className="flex justify-center">
-      <h1 className="text-white font-medium text-7xl text-center justify-center tracking-[0.2em] hidden md:block" style={{ transform: 'translateY(8rem)' }}>USLUGE</h1></div>
+      <h1 className="text-white font-medium text-7xl text-center justify-center tracking-[0.2em] hidden md:block" style={{ transform: 'translateY(8rem)' }}>SERVICES</h1></div>
       <div className="flex md:items-end lg:gap-0 justify-center h-screen">
       
-        <div className="items-center md:justify-items-center mt-12 justify-center md:grid-cols-3 md:gap-x-[2rem] lg:gap-x-[8rem] md:gap-y-2 gap-y-[1.4rem] md:grid flex-cols-3 flex flex-col lg:gap-6">
+        <div className="items-center md:justify-items-center mt-12 justify-center md:grid-cols-3 md:gap-x-[2rem] lg:gap-x-[3.5rem] md:gap-y-2 gap-y-[1.4rem] md:grid flex-cols-3 flex flex-col lg:gap-6">
         
-      <h1 className="text-white font-medium text-4xl  text-center tracking-[0.2em] block md:hidden">USLUGE</h1>
+      <h1 className="text-white font-medium text-4xl  text-center tracking-[0.2em] block md:hidden">SERVICES</h1>
            <NavLink to='/animelamps' onClick={handleClick} ><img src={anime} alt="Anime" className="color-changing-image h-auto max-w-[100px] md:max-w-[120px]" /></NavLink>
-          <h2 className="text-base font-bold text-white block md:hidden">Izrada Anime LED Lampi</h2>
+          <h2 className="text-base font-bold text-white block md:hidden">Production of Anime LED Lamps</h2>
           <NavLink to='/clocks' onClick={handleClick}><img src={clock} alt="Clock" className="color-changing-image h-auto max-w-[100px] md:max-w-[120px]" /></NavLink>
-          <h2 className="text-base font-bold text-white block md:hidden">Izrada Zidnih Satova</h2>
+          <h2 className="text-base font-bold text-white block md:hidden">Production of Wall Clocks</h2>
            <NavLink  to='/alllamps'  onClick={handleClick}><img src={alllamps} alt="All Lamps" className="color-changing-image max-w-[100px] h-auto md:max-w-[120px]" /></NavLink>
-          <h2 className="text-base font-bold text-white block md:hidden md:mb-0 mb-10">Izrada LED Lampi</h2>
-          
-        <h2 className="text-xl font-bold text-white hidden md:block">Izrada Anime LED Lampi</h2>
-        <h2 className="text-xl font-bold text-white hidden md:block">Izrada Zidnih Satova</h2>
-        <h2 className="text-xl font-bold text-white hidden md:block">Izrada LED Lampi</h2>
+          <h2 className="text-base font-bold text-white block md:hidden md:mb-0 mb-10">Production of LED Lamps</h2>
+         
+        <h2 className="text-xl font-bold text-white hidden md:block">Production of Anime LED Lamps</h2>
+        <h2 className="text-xl font-bold text-white hidden md:block">Production of Wall Clocks</h2>
+        <h2 className="text-xl font-bold text-white hidden md:block">Production of LED Lamps</h2>
 
         </div>
       </div>
